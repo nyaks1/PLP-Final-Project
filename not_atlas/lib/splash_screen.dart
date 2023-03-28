ddd8950de89537311dbcf7c702df588136806b03
Nyakallo <nyakallomasiu@gmail.com>
2023-03-28 02:32:40 +0200

    Splash screen

---
+++ not_atlas/lib/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+
+    Navigator.pushReplacement(context, '/home')});
+  }
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Not Atlas'),
+      ),
+    );
+  }
+}
