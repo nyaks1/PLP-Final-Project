411a80b228881a7c5bef3c458c256b83b110feaf
Nyakallo <nyakallomasiu@gmail.com>
2023-03-30 04:26:28 +0200

    mental health app

---
+++ not_atlas/lib/widget/communication.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CommunicationWidget extends StatefulWidget {
+  @override
+  _CommunicationWidgetState createState() => _CommunicationWidgetState();
+}
+
+class _CommunicationWidgetState extends State<CommunicationWidget> {
+  bool _voiceCall = false;
+  bool _text = false;
+  bool _videoCall = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Communication Preferences',
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 8.0),
+        CheckboxListTile(
+          title: Text('Voice Call'),
+          value: _voiceCall,
+          onChanged: (value) {
+            setState(() {
+              _voiceCall = value;
+            });
+          },
+        ),
+        CheckboxListTile(
+          title: Text('Text'),
+          value: _text,
+          onChanged: (value) {
+            setState(() {
+              _text = value;
+            });
+          },
+        ),
+        CheckboxListTile(
+          title: Text('Video Call'),
+          value: _videoCall,
+          onChanged: (value) {
+            setState(() {
+              _videoCall = value;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
